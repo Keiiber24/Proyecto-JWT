e08240ff14ab89a711c1432f593769b72f57ba25
Keiber24 <zorrillakeiber@gmail.com>
2022-09-10 03:52:14 +0000

    terminado xd

--- src/api/commands.py
+++ src/api/commands.py
@@ -8,7 +8,6 @@ Flask commands are usefull to run cronjobs or tasks outside of the API but sill 
 with youy database, for example: Import the price of bitcoin every night as 12am
 """
 def setup_commands(app):
-    
     """ 
     This is an example command "insert-test-users" that you can run from the command line
     by typing: $ flask insert-test-users 5
